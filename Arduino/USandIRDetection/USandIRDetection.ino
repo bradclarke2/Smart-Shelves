444341c23016ff04b49c188a5d18df19dd1621c6
bradclarke2 <bradclarke@hotmail.co.uk>
2017-03-17 15:58:12 +0000

    changed arduino code to be comma deliminated

--- Arduino/USandIRDetection/USandIRDetection.ino
+++ Arduino/USandIRDetection/USandIRDetection.ino
@@ -1,131 +1,86 @@
-#include <SharpIR.h>
-
-// Sonar Definitions
 // Pins
+int TRIG_PIN;
+int ECHO_PIN;
+int numberOfSensors = 9; //change to the number of sensors being used
+const int matrix [9][3] ={ //sensor,trigger,echo
+{1, 53, 52},
+{2, 51, 50},
+{3, 49, 48},
+{4, 7, 6},
+{5, 5, 4},
+{6, 3, 2},
+{7, 13, 12},
+{8, 11, 10},
+{9, 9, 8},
+}; 
 
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
 
-// IR Definitions
-#define ir A0
-#define model 1080
-SharpIR sharp(ir, 25, 93, model); // ir: sesnsor port, 25: number of readings. 93: % difference between two measurements. model: 1080 for GP2Y0A21Y or 20150 for GP2Y0A02Y
+void setup() {
+// We'll use the serial monitor to view the sensor output
+Serial.begin(9600);
 
-
-int TableSizeX = 3;
-int TableSizeY = 3;
-
-// Sonar Definitions
-const int SonarPinConfig[TableSizeX * TableSizeY][3] = 
-{ 
-{1, 1, 2},
-{2, 3, 4},
-{3, 5, 6},
-{4, 7, 8},
-{5, 9, 10},
-{6, 11, 12},
-{7, 13, 14},
-{8, 15, 16},
-{9, 17, 18},
-};
-  
-void setup(){
-  Serial.begin(9600);
-  
-  for (int h = 0; h < TableSizeX * TableSizeY; h++){
-    int SensorName = SonarPinConfig[h][0];
-    int TRIG_PIN = SonarPinConfig[h][1];
-    int ECHO_PIN = SonarPinConfig[h][2];
-    
-    pinMode(TRIG_PIN, OUTPUT);
-    digitalWrite(TRIG_PIN, LOW);
-  }
-
-  // IR Setup
-  pinMode (ir, INPUT);
+int count = 0;
+int row = 0;
+  while (count< numberOfSensors){
+  // The Trigger pin will tell the sensor to range find
+   
+  pinMode(matrix [row][1], OUTPUT);
+  digitalWrite(matrix [row][1], LOW);
+  count = count + 1;
+  row = row + 1;
+  } 
 }
 
-String getFillStatus(float sensorDistance){
-  String fillStatus;
-  if (sensorDistance > 20 ){
-   fillStatus = "0";
- }
-   else {
-   fillStatus = "1";
- }
- return fillStatus; 
- }
- 
-float SonarDistance(int TRIG_PIN, int ECHO_PIN){
-  
-   Serial.println(ECHO_PIN);
-   Serial.println(TRIG_PIN);
-   Serial.println(digitalRead(TRIG_PIN));
-  
-  unsigned long t1, t2, pulse_width;
+float SonarDistance(int TRIG_PIN, int ECHO_PIN ){
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
   float cm;
+
+  
   // Hold the trigger pin high for at least 10 us
   digitalWrite(TRIG_PIN, HIGH);
-  Serial.println(digitalRead(TRIG_PIN));
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-  Serial.println("running...");
   // Wait for pulse on echo pin
-  Serial.println(digitalRead(ECHO_PIN));
-  while ( digitalRead(ECHO_PIN) == 0 ){
-  Serial.println("running2...");
+  while ( digitalRead(ECHO_PIN) == 0 );
   // Measure how long the echo pin was held high (pulse width)
   // Note: the micros() counter will overflow after ~70 min
   t1 = micros();
-  }
   while ( digitalRead(ECHO_PIN) == 1);
   t2 = micros();
   pulse_width = t2 - t1;
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed 
+  //of sound in air at sea level (~340 m/s).
   cm = pulse_width / 58.0;
+  // Print out results
   if ( pulse_width > MAX_DIST ) {
-    Serial.println("Out of range");
-  } else {
-    Serial.print("Sonar distance: ");
-    Serial.println(cm);
-  }
-  return cm;
+    cm = 0;
+    return cm;
+  }   
+// Wait at least 60ms before next measurement
+  delay(60);
+   return cm; 
 }
 
-float IRDistance(){
-  float dis=sharp.distance();  // this returns the distance to the object you're measuring
-  Serial.print("IR distance: "); // returns it to the serial monitor
-  Serial.println(dis);
-  return dis;
+void loop() {
+
+  for (int x=0; x<numberOfSensors;x++){
+    float a = SonarDistance(matrix[x][1], matrix[x][2]) ;   
+    Serial.print(a);
+
+    if (x==(numberOfSensors -1)){
+      Serial.println();
+      break;
+    }
+    Serial.print(','); 
   }
-void loop(){
-
-  String pinMatrix[TableSizeY][TableSizeX];
-
-    int min = 0;
-    int max = 40; 
-
-   
-
-   // give each element a random number and decide if empty or full 
-   for (int i = 0; i < TableSizeY; i++) {
-       for (int j = 0; j < TableSizeX; j++) {
-          int h = 3*i + j;
-//          Serial.println(SonarPinConfig[h][0]);
-//          Serial.println(SonarPinConfig[h][1]);
-//          Serial.println(SonarPinConfig[h][2]);
-          pinMatrix[i][j] = SonarDistance(12,11);
-//          Serial.println(pinMatrix[i][j]);
-       }
-   }
-
-   Serial.println("Pin table");
-   for (int i = 0; i < TableSizeY; i++) {
-       for (int j = 0; j < TableSizeX; j++) {
-           Serial.print("  ");Serial.print(pinMatrix[i][j]);
-       }
-       Serial.println();
-   }
-   Serial.println();
  
-  delay(300000000);
- }
+  
+}
+
+
+
