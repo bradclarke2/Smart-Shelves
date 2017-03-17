0946bed8a6daa6646e6339eb9ba250078861011d
james.davenport <james.davenport>
2017-03-17 16:07:22 +0000

    Added Python code which reads values from Serial

---
+++ Python/support.py
@@ -0,0 +1,27 @@
+
+
+global sensorNumbers
+
+ifFull = "Looking Good!"
+ifEmpty = "Fill me UP!"
+
+def checkStock(sensorDistance):
+    for b in range(0,sensorNumbers):
+        if sensorDistance [b] < 20:
+            fullStatus [b] = ifFull
+        else:
+            fullStatus [b] = ifEmpty
+    print(fullStatus)
+
+numberOfItems = 0
+shelfPercentage = 0
+
+#def checkShelfPercentage(sensorDistance):
+#    global numberOfItems
+#    numberOfItems = 0
+#    for c in range(0,sensorNumbers):
+#        if fullStatus [c] == ifFull:
+#            numberOfItems = numberOfItems + 1
+#    shelfPercentage = (numberOfItems/sensorNumbers)*100
+#    print(shelfPercentage)
+#    return shelfPercentage
