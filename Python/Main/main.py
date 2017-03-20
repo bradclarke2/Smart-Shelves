12c5f8a52ad3a87b14d4932701326d3c2dcd0a27
james.davenport <james.davenport>
2017-03-20 11:13:39 +0000

    Tidied arduino and improved Pythin

--- Python/Main/main.py
+++ Python/Main/main.py
@@ -3,48 +3,47 @@ import time
 
 class USSensor(object):
     """__init__() functions as the class constructor"""
-    def __init__(self, name=None, xypos = None, trig_pin = None, echo_pin = None):
-        self.name = name
-        self.xypos = xypos
+    def __init__(self, idpos=None, xypos = None, trig_pin = None, echo_pin = None):
+        self.idpos = idpos
         self.trig_pin = trig_pin
         self.echo_pin = echo_pin
         
 class XYGrid(object):
     """__init__() functions as the class constructor"""
-    def __init__(self, name=None, trig_pin = None, echo_pin = None, distance = None):
-        self.name = name
-        self.trig_pin = trig_pin
-        self.echo_pin = echo_pin
+    def __init__(self, idpos=None, xpos = None, ypos = None, distance = None):
+        self.idpos = idpos
+        self.xpos = xpos
+        self.ypos = ypos
         self.distance = distance
         
-def MeasureDistance(USSensor): 
+def MeasureDistance(USidpos):
+    #return 1.00
+    print(USidpos)
     ser = serial.Serial('COM9')
     time.sleep(5)
-    
     while True:
         line = str(ser.readline(),'utf-8')
         line = line.strip("\r\n")
         listData = line.split(",")    
         print("line=",listData)
+        return listData[USidpos]
 
 #make a list of class USSensor(s)
 XYGridList = []
-XYGridList.append(XYGrid("0",0, 0,0.00))
-XYGridList.append(XYGrid("1",0, 1,0.00))
-XYGridList.append(XYGrid("2",0, 2,0.00))
-
-print("hello")
+XYGridList.append(XYGrid(1, 6, 7,0.00))
+XYGridList.append(XYGrid(2, 6, 7,0.00))
+XYGridList.append(XYGrid(3, 6, 7,0.00)) 
 
 USSensorList = []
-USSensorList.append(USSensor("Sensor1",0, 53, 52))
-USSensorList.append(USSensor("Sensor2",1, 51, 50))
-USSensorList.append(USSensor("Sensor3",2, 49, 48))
+USSensorList.append(USSensor(1, 6, 53, 52))
+USSensorList.append(USSensor(5, 6, 51, 50))
+USSensorList.append(USSensor(3, 6, 49, 48))
 
 for XYGrid in XYGridList:
     for USSensor in USSensorList:
-        if int(USSensor.xypos) == int(XYGrid.name):
-            print("match,xypos=",USSensor.xypos,"name=",XYGrid.name)
-            XYGrid.distance = MeasureDistance(USSensor)
+        if int(USSensor.idpos) == int(XYGrid.idpos):
+            print("match,xypos=",USSensor.idpos,"name=",XYGrid.idpos)
+            XYGrid.distance = MeasureDistance(USSensor.idpos)
             print("xygriddist=",XYGrid.distance)
         else:
-            print("nomatch,xypos=",USSensor.xypos,"name=",XYGrid.name)
+            print("nomatch,xypos=",USSensor.idpos,"name=",XYGrid.idpos)
