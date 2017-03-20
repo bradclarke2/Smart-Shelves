399c7387a5abd16931ab69bf6ca4b1f4460f13db
bradclarke2 <bradclarke@hotmail.co.uk>
2017-03-20 14:21:38 +0000

    added a check stock percentage class and calculations package

--- Python/Calculations/stockPercentage.py
+++ Python/Calculations/stockPercentage.py
@@ -1,18 +1,17 @@
+from Main.main import XYGridList
 global sensorNumbers
 
 ifFull = "Looking Good!"
 ifEmpty = "Fill me UP!"
+count =0
 
-def checkStock(sensorDistance):
-    for b in range(0,sensorNumbers):
-        if sensorDistance [b] < 20:
-            fullStatus [b] = ifFull
-        else:
-            fullStatus [b] = ifEmpty
-    print(fullStatus)
-
-numberOfItems = 0
-shelfPercentage = 0
+def checkStock(XYGrid):
+    count = 0
+    for XYGrid in XYGridList:
+        if XYGrid.distance < 20:
+            count = count +1
+    shelfPercentage = (count/XYGridList.__len__())*100
+    return shelfPercentage
 
 #def checkShelfPercentage(sensorDistance):
 #    global numberOfItems
