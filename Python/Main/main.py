3a80f43d03e875010aa779507c90c91fc7e6d4f9
james.davenport <james.davenport>
2017-03-23 13:55:23 +0000

    adding missed files

--- Python/Main/main.py
+++ Python/Main/main.py
@@ -2,16 +2,8 @@ import Calculations.stockPercentage as stockpercentages
 import Graphing.Heatmap as heatmap
 import Measurements.MeasureUS as measureUS
 import WebPage.API as startAPI
-        
-class XYGrid(object):
-    """__init__() functions as the class constructor"""
-    def __init__(self, shelflocation = None, idpos=None, xpos = None, ypos = None, distance = None):
-        self.shelflocation = shelflocation
-        self.idpos = idpos
-        self.xpos = xpos
-        self.ypos = ypos
-        self.distance = distance
-        
+import Objects.XYGrid as XYGridObject
+
 class ShelfLocationGrid(object):
     """__init__() functions as the class constructor"""
     def __init__(self, shelflocation = None, idpos=None, xpos = None, ypos = None, distance = None):
@@ -20,27 +12,8 @@ class ShelfLocationGrid(object):
         
 ShelfLocations = ["14L8E" , "15R2A"]
 
-#checkStock.checkStock(XYGrid, XYGridList)
-
-XYGridList = []
-XYGridList.append(XYGrid("14L8E", 0, 0, 0, 0.00))
-XYGridList.append(XYGrid("14L8E", 1, 0, 1, 0.00))
-XYGridList.append(XYGrid("14L8E", 2, 0, 2, 0.00)) 
-XYGridList.append(XYGrid("14L8E", 3, 1, 0, 0.00))
-XYGridList.append(XYGrid("14L8E", 4, 1, 1, 0.00))
-XYGridList.append(XYGrid("14L8E", 5, 1, 2, 0.00))
-XYGridList.append(XYGrid("14L8E", 6, 2, 0, 0.00))
-XYGridList.append(XYGrid("14L8E", 7, 2, 1, 0.00))
-XYGridList.append(XYGrid("14L8E", 8, 2, 2, 0.00)) 
-XYGridList.append(XYGrid("15R2A", 9, 0, 0, 0.00))
-XYGridList.append(XYGrid("15R2A", 10, 0, 1, 0.00))
-XYGridList.append(XYGrid("15R2A", 11, 0, 2, 0.00)) 
-XYGridList.append(XYGrid("15R2A", 12, 1, 0, 0.00))
-XYGridList.append(XYGrid("15R2A", 13, 1, 1, 0.00))
-XYGridList.append(XYGrid("15R2A", 14, 1, 2, 0.00))
-XYGridList.append(XYGrid("15R2A", 15, 2, 0, 0.00))
-XYGridList.append(XYGrid("15R2A", 16, 2, 1, 0.00))
-XYGridList.append(XYGrid("15R2A", 17, 2, 2, 0.00))   
+XYGridList = XYGridObject.MakeXYGrid()
+print(XYGridList)
     
 for singleshelfpos in ShelfLocations:
     measureUS.MeasureDistance(singleshelfpos, XYGridList)    
