94c0067ba14583f163e245121d18d7d4464d12f2
james.davenport <james.davenport>
2017-04-07 14:40:54 +0100

    not working

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,10 @@
 from configparser import SafeConfigParser
+import os
 
 parser = SafeConfigParser()
-parser.read('example.ini')
+parser.read('keys.ini')
+
+print(os.path.abspath('keys.ini'))
 
 key = parser.get('credentials', 'APIkey')
 print(key)
